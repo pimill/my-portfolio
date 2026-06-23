22b276eae306dea771de8f394e7a9c03be308a67
pimill <s112001146@g.ksu.edu.tw>
2026-06-24 01:09:30 +0800

    Update ProjectModal.tsx

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -15,9 +15,11 @@ export const ProjectModal = ({ project, onClose }: ProjectModalProps) => {
   useEffect(() => {
     if (project) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     }
     return () => {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     };
   }, [project]);
 
