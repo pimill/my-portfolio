4cf22987bcc524ec8e64ac7889373b12894aa537
pimill <s112001146@g.ksu.edu.tw>
2026-06-25 01:24:14 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,12 @@ export interface Project {
   id: string;
   title: string;
   category: string;
-  coverImage: string;
-  heroImage: string;
   description: string;
   concept?: string;
   specs?: string[];
   colorPalette?: string[];
   process?: string[];
   outcomes?: string[];
+  heroImage: string;
+  coverImage: string;
 }
