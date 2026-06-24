8a6b1c4a51c438a2395342ee901933567995c323
pimill <s112001146@g.ksu.edu.tw>
2026-06-25 03:16:53 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Loader } from './components/Loader';
 import { Hero } from './components/Hero';
 import { Navigation } from './components/Navigation';
 import { Works } from './components/Works';
-import { About } from './components/About';
+// 這裡移除了 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { ScrollToTop } from './components/ScrollToTop';
 
@@ -18,7 +18,7 @@ export default function App() {
       <main>
         <Hero />
         <Works />
-        <About />
+        {/* 這裡把 <About /> 刪除了！因為現在改用 Navigation 裡面的彈出視窗 */}
         <Contact />
       </main>
       <ScrollToTop />
