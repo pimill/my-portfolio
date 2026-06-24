1cfd2132cc88d82b7831b2c5e3a149e5202c25cb
pimill <s112001146@g.ksu.edu.tw>
2026-06-24 19:50:10 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useState } from 'react';
 import { Loader } from './components/Loader';
 import { Hero } from './components/Hero';
 import { Navigation } from './components/Navigation';
@@ -10,17 +11,26 @@ import { Works } from './components/Works';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { ScrollToTop } from './components/ScrollToTop';
+import { PastWorks } from './components/PastWorks';
 
 export default function App() {
+  const [view, setView] = useState<'home' | 'past-works'>('home');
+
   return (
     <div className="min-h-screen bg-white">
       <Loader />
-      <Navigation />
+      <Navigation currentView={view} onViewChange={setView} />
       <main>
-        <Hero />
-        <Works />
-        <About />
-        <Contact />
+        {view === 'home' ? (
+          <>
+            <Hero />
+            <Works onViewChange={setView} />
+            <About />
+            <Contact />
+          </>
+        ) : (
+          <PastWorks onViewChange={setView} />
+        )}
       </main>
       <ScrollToTop />
     </div>
